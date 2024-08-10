27183cac969fabea0d1429bbb4bf040478f244a1
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-08-10 12:48:08 +0530

    Navigation using Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function Home() {
   return (
     <div>
       <h1>Hello Nextjs!</h1>
+      <ul>
+        <li>
+          <Link href={`/about`}>Navigate to about page</Link>
+        </li>
+        <li>
+          <Link href={`/`}>Navigate to home page</Link>
+        </li>
+        <li>
+          <Link href={`/account/membership`}>Navigate to membership page</Link>
+        </li>
+      </ul>
     </div>
   );
 }
